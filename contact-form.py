8ada4bf5e8b7ad11eb20653765c7676c19d096a7
sandrineay <sandrineayral@sandrines-air.lan>
2016-07-27 15:37:00 +0100

    Complete content + review python code for the form

--- contact-form.py
+++ contact-form.py
@@ -5,17 +5,18 @@ from flask import render_template
 app = Flask("MyApp")
 
 
-@app.route("/")
+@app.route("/contact")
 def contact():
 	return render_template("index.html")
 
 
-@app.route("/", methods=['POST'])
+@app.route("/contact", methods=['POST'])
 def sign_up():
 	form_data = request.form
 	print form_data['name']
 	print form_data['email']
 	print form_data['message']
 	print form_data['url']
+	return "Merci!"
 	
 app.run()

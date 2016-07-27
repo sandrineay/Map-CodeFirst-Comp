60c42b5aea26b7ce95ad2bb3909579d88f991cd6
sandrineay <sandrineayral@sandrines-air.lan>
2016-07-27 13:04:44 +0100

    Changing python file

--- contact-form.py
+++ contact-form.py
@@ -5,18 +5,17 @@ from flask import render_template
 app = Flask("MyApp")
 
 
-@app.route("/contact")
+@app.route("/")
 def contact():
 	return render_template("index.html")
 
 
-@app.route("/contact", methods=['POST'])
+@app.route("/", methods=['POST'])
 def sign_up():
 	form_data = request.form
 	print form_data['name']
 	print form_data['email']
 	print form_data['message']
 	print form_data['url']
-	return "All OK"
 	
 app.run()
